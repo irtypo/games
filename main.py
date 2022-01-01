625ed1ecba8f0ba3504fe4888654f28d5539d4d4
irtypo <irtypo@gmail.com>
2022-01-01 00:29:34 -0600

    main refactor

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ from metadata import MetaData
 from hud import HeadsUpDisplay as HUD
 from pong.scoreboard import ScoreBoard
 
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720 + (720 * .2)
+WINDOW_ICON = pg.image.load('common\src\\brain.png')
+AVAILABLE_GAMES = ['menu', 'pong', 'drive', 'billards', '4']
 
+metaData = MetaData()
+mouseY = 10
 
 def main():
-    WINDOW_WIDTH = 1280
-    WINDOW_HEIGHT = 720 + (720 * .2)
-    WINDOW_ICON = pg.image.load('common\src\\brain.png')
-    AVAILABLE_GAMES = ['menu', 'pong', 'drive', 'billards', '4']
 
     pg.init()
     pg.time.Clock()
-    metaData = MetaData()
     metaData.gameList = AVAILABLE_GAMES
     metaData.gameName = metaData.gameList[0]
     metaData.displayText = metaData.gameName
-    mouseY = 10
 
     surface = pg.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
     pg.display.set_caption("ping")
@@ -36,44 +36,17 @@ def main():
 
         # events
         for event in pg.event.get():
-            # if event == "down":
-            # print(event)
 
-            
-            # close window
-            if event.type == pg.QUIT:
-                metaData.gameOver = True
-            # ctrl + c
-            elif event.type == pg.KEYDOWN:
-                if event.mod == 4160 and event.key == 99:
-                    metaData.gameOver = True
+            if metaData.gameName == 'drive':
+                driveControls(drive, event)
 
-                # go back to the menu
-                if metaData.gameName != 'menu':
-                    if event.type == pg.KEYDOWN:
-                        if event.key == K_ESCAPE:
-                            metaData.gameName = 'menu'
+            elif metaData.gameName == 'pong':
+                pongControls(pong, event)
 
-                if metaData.gameName == 'drive':
-                    if event.key == pg.K_SPACE:
-                        drive.car.jumpStart()
-                        
-
-            elif event.type == pg.MOUSEBUTTONUP:
-                if metaData.gameName == 'menu' and menu.click(event) != None:
-                    metaData.gameName = menu.click(event)
+            menuControls(menu, event)
 
 
-            elif event.type == pg.MOUSEMOTION:
-                if metaData.gameName == 'pong':
-                    if event.pos[1] != None:
-                        mouseY = event.pos[1]
 
-
-            elif event.type == pg.KEYUP:
-                if metaData.gameName == 'drive':
-                    if event.key == pg.K_SPACE:
-                        drive.car.jumpStop()
         # drawing
         pg.display.update()
         surface.fill((0,0,0))
@@ -81,6 +54,7 @@ def main():
             pg.mouse.set_visible(True)
             menu.draw()
             hud.draw(metaData)
+
         elif metaData.gameName == 'pong':
             if mouseY > WINDOW_HEIGHT-hud.maxHeight:
                 pg.mouse.set_visible(True)
@@ -89,14 +63,57 @@ def main():
 
             pong.draw(mouseY, metaData)
             sb.draw(metaData)
+        
         # elif metaData.gameName == 'breaker':
             # pass
+
         elif metaData.gameName == 'drive':
             drive.draw(metaData)
             hud.draw(metaData)
 
+
+
+
     pg.quit()
+
+def driveControls(drive, e):
+    if e.type == pg.MOUSEBUTTONDOWN or (e.type == pg.KEYDOWN and e.key == pg.K_SPACE):
+        drive.car.jumpStart()
+
+    if e.type == pg.MOUSEBUTTONUP or (e.type == pg.KEYUP and e.key == pg.K_SPACE):
+        drive.car.jumpStop()
+
+def pongControls(pong, e):
+    global mouseY
+    if e.type == pg.MOUSEMOTION:
+        if e.pos[1] != None:
+            mouseY = e.pos[1]
+
+
+def menuControls(menu, e):
+    # close window
+    if e.type == pg.QUIT:
+        metaData.gameOver = True
+        
+    # go back to the menu
+    if metaData.gameName != 'menu':
+        if e.type == pg.KEYDOWN:
+            if e.key == K_ESCAPE:
+                metaData.gameName = 'menu'
+
+    # ctrl + c
+    if e.type == pg.KEYDOWN:
+        if e.mod and pg.KMOD_LCTRL and e.key == pg.K_c:
+            metaData.gameOver = True
+
+    # select game
+    if e.type == pg.MOUSEBUTTONUP:
+        if metaData.gameName == 'menu' and menu.click(e) != None:
+            metaData.gameName = menu.click(e)
+
 
 
 if __name__=="__main__":
     main()
+
+
